f863c84b5b5efb16715f786dd63af879ce60a2a7
Rico <v_fharsono@tencent.com>
2022-11-18 17:45:36 +0800

    edit time

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.get("/Dota2", (req, res) => {
   fs.readFile(`${__dirname}/data/Dota.json`, (err, data) => {
@@ -20,6 +20,13 @@ app.get("/CS:GO", (req, res) => {
   fs.readFile(`${__dirname}/data/CSGO.json`, (err, data) => {
     if (err) { console.log(err) }
     let results = JSON.parse(data);
+
+    results = results.filter( result => {
+      let now = new Date().getTime();
+      let future = Date.parse(result.time)
+      return future - now;
+    })
+
     res.render("index", { results });
   });
 });
