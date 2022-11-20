aa440d1ca93173604f35f6807321ca289f2b176e
Shuliii <ricoharsono@gmail.com>
2022-11-20 18:36:14 +0800

    edit valorant odds and soccer odds

--- routes/soccer.js
+++ routes/soccer.js
@@ -19,8 +19,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/submit", (req, res) => {
-  console.log(req.originalUrl);
-  console.log(req.body);
+  let newbetamount = Number(req.body.betamount);
+  let newoddsamount = Number(req.body.odds);
+  const queryString = `INSERT INTO BETLIST (ID, Username, Match_Name, Bet_Name, Amount, Odds) values (NULL, '${req.body.username}','${req.body.matchName}', '${req.body.betName}', ${newbetamount}, ${newoddsamount})`;
+  console.log(queryString);
+  connection.query(queryString, (err, result) => {
+    err ? console.log(err) : console.log("successful");
+  });
+
   res.redirect("/Soccer");
 });
 
