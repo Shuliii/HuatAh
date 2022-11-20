34c4b6546b6a81105579b65f1d04dad2be90276f
Shuliii <ricoharsono@gmail.com>
2022-11-20 23:23:15 +0800

    edit nba data

--- routes/nba.js
+++ routes/nba.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const connection = require("./db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -22,7 +23,7 @@ router.get("/", (req, res) => {
 router.post("/submit", (req, res) => {
   let newbetamount = Number(req.body.betamount);
   let newoddsamount = Number(req.body.odds);
-  const queryString = `INSERT INTO BETLIST values (NULL, '${req.body.username}','${req.body.matchName}', '${req.body.betName}', ${newbetamount}, ${newoddsamount})`;
+  const queryString = `INSERT INTO BETLIST (ID, Username, Match_Name, Bet_Name, Amount, Odds) values (NULL, '${req.body.username}','${req.body.matchName}', '${req.body.betName}', ${newbetamount}, ${newoddsamount})`;
   console.log(queryString);
   connection.query(queryString, (err, result) => {
     err ? console.log(err) : console.log("successful");
