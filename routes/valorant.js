65f6486fe5574ab78756dfcfea711c5baae1f3f1
Shuliii <ricoharsono@gmail.com>
2022-11-20 12:45:42 +0800

    edit script

--- routes/valorant.js
+++ routes/valorant.js
@@ -22,7 +22,7 @@ router.get("/", (req, res) => {
 router.post("/submit", (req, res) => {
   let newbetamount = Number(req.body.betamount);
   let newoddsamount = Number(req.body.odds);
-  const queryString = `INSERT INTO BETLIST values (NULL, '${req.body.username}','${req.body.matchName}', '${req.body.betName}', ${newbetamount}, ${newoddsamount})`;
+  const queryString = `INSERT INTO BETLIST (ID, Username, Match_Name, Bet_Name, Amount, Odds) values (NULL, '${req.body.username}','${req.body.matchName}', '${req.body.betName}', ${newbetamount}, ${newoddsamount})`;
   console.log(queryString);
   connection.query(queryString, (err, result) => {
     err ? console.log(err) : console.log("successful");
