3b108ac63017e2be98e16d2f74a4a01a173d21e7
Rico <v_fharsono@tencent.com>
2022-11-21 10:03:29 +0800

    edit odds soccer

--- update.sql
+++ update.sql
@@ -1,5 +1,9 @@
-update betlist set Balance = Amount*-1 where Bet_Result = 'L';
-update betlist set Balance = 0 where Bet_Result = 'NA';
-update betlist set Balance = (Odds - 1) * Amount where Bet_Result = 'W';
+update betlist set Balance = Amount*-1 where Bet_Result = 'L' AND Balance is NULL;
+update betlist set Balance = 0 where Bet_Result = 'NA' AND Balance is NULL;
+update betlist set Balance = (Odds - 1) * Amount where Bet_Result = 'W' AND Balance is NULL;
 
 select * from betlist;
+
+SET SQL_SAFE_UPDATES = 0;
+
+update betlist set Match_Result = '13-5' where Match_Name = 'G2 vs SR Map5';
