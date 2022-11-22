f43a4c2dcc1cd79d99ae3a70d9becf7c343a63d3
Rico <v_fharsono@tencent.com>
2022-11-22 16:44:26 +0800

    edit searchresult

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ app.use("/Valorant", valorantRouter);
 app.use("/NBA", nbaRouter);
 app.use("/Soccer", soccerRouter);
 
-app.post("/find", (req, res) => {
-  const queryString = `SELECT * FROM BETLIST where Username = '${req.body.firstName}'`;
-  connection.query(queryString, (err, data) => {
-    err ? console.log(err) : console.log(data);
-    res.render("index", { data });
+app.post("/search", (req, res) => {
+  const queryString = `SELECT * FROM BETLIST where Username = '${req.body.firstName}' and Match_Result is NULL`;
+  connection.query(queryString, (err, results) => {
+    err && console.log(err)
+    res.render("searchResult.ejs", { results });
   });
 });
 
 app.get("/search", (req, res) => {
-  res.render("search");
+  res.render("search.ejs");
 });
 
 app.listen(port, () => {
@@ -42,3 +42,58 @@ app.listen(port, () => {
 //     <h1><%= el.Match_Name %></h1>
 //   </div>
 //   <% }) %>
+
+{/* <div class="search-content">
+        <!-- <div class="search-balance">
+          test1
+        </div>
+        <div class="search-active-bets">
+          <h1>Active Bets</h1>
+          <div class="active-bets">
+            <h2>Team1 vs Team2</h2>
+            <div class="active-bets-details">
+              <h3>Betting on: <span>Team1 Winner</span></h3>
+              <h3>Amount: <span>20</span></h3>
+              <h3>Odds: <span>1.93</span></h3>
+            </div>
+            <div class="potential">
+              <h3>Potential Winnings: <span>18.6</span></h3>
+            </div>
+          </div>
+          <div class="active-bets">
+            <h2>Team1 vs Team2</h2>
+            <div class="active-bets-details">
+              <h3>Betting on: <span>Team1 Winner</span></h3>
+              <h3>Amount: <span>20</span></h3>
+              <h3>Odds: <span>1.93</span></h3>
+            </div>
+            <div class="potential">
+              <h3>Potential Winnings: <span>18.6</span></h3>
+            </div>
+          </div>
+          <% results.forEach((result) => { %>
+            <div class="active-bets">
+              <h2><%= result.Match_Name %></h2>
+              <div class="active-bets-details">
+                <h3>Betting on: <span><%= result.Bet_Name %></span></h3>
+                <h3>Amount: <span><%= result.Amount %></span></h3>
+                <h3>Odds: <span><%= result.Odds %></span></h3>
+              </div>
+              <div class="potential">
+                <h3>Potential Winnings: <span><% result.Amount * (result.Odds - 1) %></span></h3>
+              </div>
+            </div>
+          <% }) %>
+          <div class="active-bets">
+            <h2>Team1 vs Team2</h2>
+            <div class="active-bets-details">
+              <h3>Betting on: <span>Team1 Winner</span></h3>
+              <h3>Amount: <span>20</span></h3>
+              <h3>Odds: <span>1.93</span></h3>
+            </div>
+            <div class="potential">
+              <h3>Potential Winnings: <span>18.6</span></h3>
+            </div>
+          </div>
+        </div> -->
+      </div> */}
