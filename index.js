34bab54eb1fde35d4758b6aeb81b15ac4e89f8d1
Shuliii <ricoharsono@gmail.com>
2022-11-25 01:01:31 +0800

    edit odds and enable balance

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connection = require("./routes/db");
+const async = require("async");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -22,11 +23,29 @@ app.use("/NBA", nbaRouter);
 app.use("/Soccer", soccerRouter);
 
 app.post("/search", (req, res) => {
-  const queryString = `SELECT * FROM BETLIST where Username = '${req.body.firstName}' and Match_Result is NULL`;
-  connection.query(queryString, (err, results) => {
-    err && console.log(err);
-    res.render("searchResult.ejs", { results });
-  });
+  const queryString1 =
+    "SELECT * FROM betlist WHERE Username = ? and Match_Result is NULL";
+  const queryString2 =
+    "select SUM(Balance) as balance from betlist where Username = ?";
+  const queryString3 = `SELECT * FROM BETLIST where Username = '${req.body.firstName}' and Match_Result is NULL`;
+  async.parallel(
+    [
+      function (callback) {
+        connection.query(queryString2, [req.body.firstName], (err, result) => {
+          callback(null, result);
+        });
+      },
+      function (callback) {
+        connection.query(queryString1, [req.body.firstName], (err, result) => {
+          callback(null, result);
+        });
+      },
+    ],
+    function (err, results) {
+      // console.log(results);
+      res.render("searchResult.ejs", { results });
+    }
+  );
 });
 
 app.get("/search", (req, res) => {
@@ -36,3 +55,44 @@ app.get("/search", (req, res) => {
 app.listen(port, () => {
   console.log("Running on port 3000");
 });
+
+// const queryString = `SELECT * FROM BETLIST where Username = '${req.body.firstName}' and Match_Result is NULL`;
+//   connection.query(queryString, (err, results) => {
+//     err && console.log(err);
+//     res.render("searchResult.ejs", { results });
+
+// [
+//   RowDataPacket {
+//     ID: 2964,
+//     Username: 'Vito',
+//     Match_Name: 'Portugal vs Ghana',
+//     Bet_Name: 'Ghana Winner',
+//     Amount: 5,
+//     Odds: 9,
+//     Match_Result: null,
+//     Bet_Result: null,
+//     Balance: null
+//   },
+//   RowDataPacket {
+//     ID: 2974,
+//     Username: 'Vito',
+//     Match_Name: 'Portugal vs Ghana',
+//     Bet_Name: 'Draw',
+//     Amount: 10,
+//     Odds: 4.12,
+//     Match_Result: null,
+//     Bet_Result: null,
+//     Balance: null
+//   },
+//   RowDataPacket {
+//     ID: 2984,
+//     Username: 'Vito',
+//     Match_Name: 'Portugal vs Ghana',
+//     Bet_Name: 'Under 3',
+//     Amount: 10,
+//     Odds: 1.92,
+//     Match_Result: null,
+//     Bet_Result: null,
+//     Balance: null
+//   }
+// ]
