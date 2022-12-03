7234cbf7cc55f4a857a41bb7d07d249935c52274
Shuliii <ricoharsono@gmail.com>
2022-12-03 11:27:41 +0800

    edit to add functionality to check time

--- routes/nba.js
+++ routes/nba.js
@@ -21,12 +21,27 @@ router.get("/", (req, res) => {
 router.post("/submit", (req, res) => {
   let newbetamount = Number(req.body.betamount);
   let newoddsamount = Number(req.body.odds);
-  const queryString = `INSERT INTO BETLIST (ID, Username, Match_Name, Bet_Name, Amount, Odds) values (NULL, '${req.body.username}','${req.body.matchName}', '${req.body.betName}', ${newbetamount}, ${newoddsamount})`;
-  console.log(queryString);
-  connection.query(queryString, (err, result) => {
-    err ? console.log(err) : console.log("successful");
+  const sthElse = path.join(__dirname, "/../data/NBA.json");
+
+  fs.readFile(`${sthElse}`, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    let results = JSON.parse(data);
+    results = results.filter(
+      (object) => object.match_name == req.body.matchName
+    );
+
+    if (Date.parse(results[0].time) > new Date().getTime()) {
+      const queryString = `INSERT INTO BETLIST (ID, Username, Match_Name, Bet_Name, Amount, Odds) values (NULL, '${req.body.username}','${req.body.matchName}', '${req.body.betName}', ${newbetamount}, ${newoddsamount})`;
+      connection.query(queryString, (err, result) => {
+        err ? console.log(err) : console.log("successful");
+      });
+      res.redirect("/NBA");
+    } else {
+      res.redirect("/NBA");
+    }
   });
-  res.redirect("/NBA");
 });
 
 module.exports = router;
