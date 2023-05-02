779d77ce8d18ec54d05174b9c67493f5ca7d1b6b
Shuliii <ricoharsono@gmail.com>
2023-05-02 22:06:54 +0800

    edit odds

--- update.sql
+++ update.sql
@@ -7,3 +7,6 @@ select * from betlist;
 SET SQL_SAFE_UPDATES = 0;
 
 update betlist set Match_Result = '13-5' where Match_Name = 'G2 vs SR Map5';
+
+insert into betlist values(null, 'Mcspicy', 'Forze vs L4D Match', 'Map Score 2-1', 50, 3.38, null, null, null);
+insert into betlist values(null, 'nk1', 'Forze vs L4D Match', 'Map Score 2-1', 10, 3.38, null, null, null);
