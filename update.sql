5443093b57e18de58c6c21cf4177adc5993d69a3
Shuliii <ricoharsono@gmail.com>
2023-05-16 09:37:39 +0800

    edit

--- update.sql
+++ update.sql
@@ -9,4 +9,6 @@ SET SQL_SAFE_UPDATES = 0;
 update betlist set Match_Result = '13-5' where Match_Name = 'G2 vs SR Map5';
 
 insert into betlist values(null, 'Mcspicy', 'Forze vs L4D Match', 'Map Score 2-1', 50, 3.38, null, null, null);
-insert into betlist values(null, 'nk1', 'Forze vs L4D Match', 'Map Score 2-1', 10, 3.38, null, null, null);
+insert into betlist values(null, 'nk1', 'Many many bets', 'Win', 101.95, 2.00, null, null, null);
+
+update betlist set Match_Result = '' where Match_Name = '' AND Bet_Result is Null;
